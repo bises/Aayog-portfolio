952b79b10be96a1f4c29147843168f802428a6cf
Bishesh Acharya <acharya.bises@outlook.com>
2019-09-29 17:06:01 -0400

    update deploy.cmd 3

--- deploy.cmd
+++ deploy.cmd
@@ -103,7 +103,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\my-portfolio\package.json" (
 IF EXIST "%DEPLOYMENT_SOURCE%\my-portfolio\.angular-cli.json" (
 echo Building App in %DEPLOYMENT_SOURCE%…
 pushd "%DEPLOYMENT_SOURCE%"
-call :ExecuteCmd !NPM_CMD! run build
+call :ExecuteCmd !NPM_CMD! run build --prod
 :: If the above command fails comment above and uncomment below one
 :: call ./node_modules/.bin/ng build –prod
 IF !ERRORLEVEL! NEQ 0 goto error
